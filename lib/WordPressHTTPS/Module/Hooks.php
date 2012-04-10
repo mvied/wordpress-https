8c5172060b825e4e769729add7fd9d163f13d3d5
Mike Ems <mike@mvied.com>
2012-04-10 15:01:06 -0500

    Updated cookie expiration definition to use modify method rather than add on the DateTime object.

--- lib/WordPressHTTPS/Module/Hooks.php
+++ lib/WordPressHTTPS/Module/Hooks.php
@@ -70,7 +70,7 @@ class WordPressHTTPS_Module_Hooks extends WordPressHTTPS_Module implements WordP
 	public function proxy_check() {
 		$cookie_expiration = new DateTime;
 		$cookie_expiration->setTimezone(new DateTimeZone('GMT'));
-		$cookie_expiration = $cookie_expiration->add(new DateInterval('P10Y'))->format('D, d-M-Y H:i:s') . ' GMT';
+		$cookie_expiration = $cookie_expiration->modify(new DateInterval('P10Y'))->format('D, d-M-Y H:i:s') . ' GMT';
 
 		echo '<!-- WordPress HTTPS Proxy Check -->';
 		echo '<script type="text/javascript">function getCookie(a){var b=document.cookie;var c=a+"=";var d=b.indexOf("; "+c);if(d==-1){d=b.indexOf(c);if(d!=0)return null}else{d+=2;var e=document.cookie.indexOf(";",d);if(e==-1){e=b.length}}return unescape(b.substring(d+c.length,e))}if(getCookie("wp_proxy")!=true){if(window.location.protocol=="https:"){document.cookie="wp_proxy=1; path=/; expires=' . $cookie_expiration . '"}else if(getCookie("wp_proxy")==null){document.cookie="wp_proxy=0; path=/; expires=' . $cookie_expiration . '"}if(getCookie("wp_proxy")!=null){window.location.reload()}}</script>';
