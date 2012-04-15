d816fa45d46e5ae2a882ecf2d88437809f569342
Mike Ems <mike@mvied.com>
2012-04-15 13:00:22 -0500

    Simplified URL matching regex's.

--- lib/WordPressHTTPS/Url.php
+++ lib/WordPressHTTPS/Url.php
@@ -442,9 +442,9 @@ class WordPressHTTPS_Url {
 	public static function fromString( $string ) {
 		$url = new WordPressHTTPS_Url;
 
-		@preg_match_all('/(http|https):\/\/[\/-\w\d\.,~#@^!\'()?=\+&%;:[\]]+/i', $string, $url_parts);
-		if ( isset($url_parts[0][0]) ) {
-			if ( $url_parts = parse_url( $url_parts[0][0] ) ) {
+		@preg_match_all('/((http|https):\/\/[^\'"]+)[\'"]?/i', $string, $url_parts);
+		if ( isset($url_parts[1][0]) ) {
+			if ( $url_parts = parse_url( $url_parts[1][0] ) ) {
 				foreach( $url_parts as $key => $value ) {
 					$property = '_' . $key;
 					$camelCase = create_function('$c', 'return strtoupper($c[1]);');
