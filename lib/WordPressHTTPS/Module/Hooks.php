151877174efa367560c72ffa25855bcfa848e593
Mike Ems <mike@mvied.com>
2012-04-16 18:17:36 -0500

    Removed use of DateTime object due to PHP 5.2 compatibility issue. Trusty strtotime instead.

--- lib/WordPressHTTPS/Module/Hooks.php
+++ lib/WordPressHTTPS/Module/Hooks.php
@@ -55,10 +55,7 @@ class WordPressHTTPS_Module_Hooks extends WordPressHTTPS_Module implements WordP
 	 * @return void
 	 */
 	public function proxy_check() {
-		$cookie_expiration = new DateTime;
-		$cookie_expiration->setTimezone(new DateTimeZone('GMT'));
-		$cookie_expiration = $cookie_expiration->modify('+10 years')->format('D, d-M-Y H:i:s') . ' GMT';
-
+		$cookie_expiration = gmdate('D, d-M-Y H:i:s T', strtotime('now + 10 years'));
 		echo '<!-- WordPress HTTPS Proxy Check -->';
 		echo '<script type="text/javascript">function getCookie(a){var b=document.cookie;var c=a+"=";var d=b.indexOf("; "+c);if(d==-1){d=b.indexOf(c);if(d!=0)return null}else{d+=2;var e=document.cookie.indexOf(";",d);if(e==-1){e=b.length}}return unescape(b.substring(d+c.length,e))}if(getCookie("wp_proxy")!=true){if(window.location.protocol=="https:"){document.cookie="wp_proxy=1; path=/; expires=' . $cookie_expiration . '"}else if(getCookie("wp_proxy")==null){document.cookie="wp_proxy=0; path=/; expires=' . $cookie_expiration . '"}if(getCookie("wp_proxy")!=null){window.location.reload()}else{document.write("You must enable cookies.")}}</script>';
 		echo '<noscript>Your browser does not support JavaScript.</noscript>';
