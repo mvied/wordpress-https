26ca49dbc8f8fd0c9fadb6ece5aab14c24147544
Christian Foellmann <foellmann@foe-services.de>
2012-12-18 13:35:15 +0100

    German language + minimal interface improvements

--- admin/templates/metabox/post.php
+++ admin/templates/metabox/post.php
@@ -21,7 +21,7 @@ wp_nonce_field($this->getPlugin()->getSlug(), $this->getPlugin()->getSlug());
 <?php if ( isset($parent_force_ssl_children) ) { ?>
 	<input type="hidden" value="<?php echo ( $force_ssl  ? 1 : 0 ); ?>" name="force_ssl" />
 <?php } ?>
-	<label<?php echo ( isset($parent_force_ssl_children)  ? ' title="' . __('This post\'s parent page \'','wordpress-https') . $parent_force_ssl_children->post_title . __('\' has \'Secure child posts\' enabled.','wordpress-https') . '"' : '' ); ?>><input type="checkbox" value="1" name="force_ssl" <?php echo ( $force_ssl  ? ' checked="checked"' : '' ); ?><?php echo ( isset($parent_force_ssl_children)  ? ' disabled="disabled="' : '' ); ?> /> <?php _e('Secure post','wordpress-https'); ?></label>
+	<label<?php echo ( isset($parent_force_ssl_children)  ? ' title="' . printf( __('This post\'s parent page \' %s \' has \'Secure child posts\' enabled.','wordpress-https'), $parent_force_ssl_children->post_title) . '"' : '' ); ?>><input type="checkbox" value="1" name="force_ssl" <?php echo ( $force_ssl  ? ' checked="checked"' : '' ); ?><?php echo ( isset($parent_force_ssl_children)  ? ' disabled="disabled="' : '' ); ?> /> <?php _e('Secure post','wordpress-https'); ?></label>
 </div>
 <div class="misc-pub-section misc-pub-section-last">
 	<label><input type="checkbox" value="1" name="force_ssl_children" <?php echo ( $force_ssl_children  ? ' checked="checked"' : '' ); ?> /> <?php _e('Secure child posts','wordpress-https'); ?></label>
