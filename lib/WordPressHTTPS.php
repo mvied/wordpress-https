a177acffa1d68d3c648e50ec53c91aaac6a5a3e3
Mike Ems <mike@mvied.com>
2013-03-27 10:29:42 -0500

    Added conitional in case HTTP_X_FORWARDED_SSL == 1.

--- lib/WordPressHTTPS.php
+++ lib/WordPressHTTPS.php
@@ -424,7 +424,7 @@ class WordPressHTTPS extends Mvied_Plugin {
 			return false;
 		} else if ( isset($_SERVER['HTTP_CF_VISITOR']) && strpos($_SERVER['HTTP_CF_VISITOR'], 'https') ) {
 			return true;
-		} else if ( isset($_SERVER['HTTP_X_FORWARDED_SSL']) && strtolower($_SERVER['HTTP_X_FORWARDED_SSL']) == 'on' ) {
+		} else if ( isset($_SERVER['HTTP_X_FORWARDED_SSL']) && ( strtolower($_SERVER['HTTP_X_FORWARDED_SSL']) == 'on' || $_SERVER['HTTP_X_FORWARDED_SSL'] == 1 ) ) {
 			return true;
 		} else if ( isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && strtolower($_SERVER['HTTP_X_FORWARDED_PROTO']) == 'https' ) {
 			return true;
