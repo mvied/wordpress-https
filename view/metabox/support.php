1c1b96f0511254dcf98e9369ff8cd7ab9fd65d01
Mike Ems <mike@mvied.com>
2017-12-29 16:37:09 -0600

    New support topic link update.

--- view/metabox/support.php
+++ view/metabox/support.php
@@ -3,4 +3,4 @@
 	<li>Please read the introductory <a href="http://wordpress.org/extend/plugins/wordpress-https/installation/" target="_blank">Installation Guide</a>.</li>
 	<li>If you haven't already, check out the <a href="http://wordpress.org/extend/plugins/wordpress-https/faq/" target="_blank">Frequently Asked Questions</a>.</li>
 </ul>
-<p>Still not fixed? Please <a href="http://wordpress.org/support/plugin/wordpress-https" target="_blank">start a support topic</a> and someone from the community may be able to assist you. If you need immediate assistance, I am available for hire. Unfortunately, I do not have time to support the plugin for free.</p>
+<p>Still not fixed? Please <a href="https://wordpress.org/support/plugin/wordpress-https#new-post" target="_blank">start a support topic</a> and someone from the community may be able to assist you. If you need immediate assistance, I am available for hire. Unfortunately, I do not have time to support the plugin for free.</p>
